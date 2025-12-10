32c2408c5da6fe38fdaaa374dc4819ac0f8cbf54
safatarek <se2400552@tkh.edu.eg>
2025-12-10 23:14:42 +0200

    added comments

--- csMainFile.py
+++ csMainFile.py
@@ -1,21 +1,21 @@
-from cskeys import genkey, gen16keys
+from cskeys import genkey, gen16keys #to avoid rewriting the functions
 from csEncryption import EncryptedMessage
 from csDecryption import DecryptedMessage
 
 def Main():
-    key64=genkey()
+    key64=genkey() #generates keys
     print('Random 64-bit key:')
     print(key64)
 
     roundkeys=gen16keys(key64)
 
-    PlainText=input('Please enter your message to get encrypted:')
+    PlainText=input('Please enter your message to get encrypted:') #gets the main text from the user
 
-    CipherBits=EncryptedMessage(PlainText, roundkeys)
+    CipherBits=EncryptedMessage(PlainText, roundkeys) #encrypts the text
     print('Ciphertext Bits:')
     print (CipherBits)
 
-    DecryptedText=DecryptedMessage(CipherBits, roundkeys)
+    DecryptedText=DecryptedMessage(CipherBits, roundkeys) #decrypts it back into original
     print('Decrypted Text:')
     print(DecryptedText)
 
