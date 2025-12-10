d61eb48338bb9e4b3cf8acdd1b1c4db07a7f7c05
safatarek <se2400552@tkh.edu.eg>
2025-12-10 23:13:28 +0200

    added the comments

--- cskeys.py
+++ cskeys.py
@@ -7,7 +7,7 @@ PC1= [
      7,62,54,46,38,30,22,
     14, 6,61,53,45,37,29,
     21,13, 5,28,20,12, 4
-]
+] #permutation tables
 
 PC2= [
      14,17,11,24, 1, 5,
@@ -19,17 +19,17 @@ PC2= [
     44,49,39,56,34,53,
     46,42,50,36,29,32
 ]
-shiftby=[1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,1]
+shiftby=[1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,1] #shift schedule
 
-import random
+import random #so it wont be one set key
 
-def genkey():
+def genkey(): #key generation
     key= ''
     for k in range(64):
         key+=str (random.randint(0,1))
     return key
 
-def permute(bits, table): 
+def permute(bits, table): #to order the bits
     result=''
     for index in table:
         result+=bits [index - 1]
@@ -39,17 +39,17 @@ def leftshiftbt28(bt28, p):
     return bt28[p:]+bt28[:p]
 
 def gen16keys(keybits):
-    key56=permute(keybits, PC1)
-    C=key56[:28]
+    key56=permute(keybits, PC1) #to remove parity bits =56 bits
+    C=key56[:28] #splits them to 2 parts
     D=key56[28:]
 
-    keys16=[]
+    keys16=[] #to generate 16 iterations
 
     for shiftn in shiftby:
         C=leftshiftbt28(C,shiftn)
         D=leftshiftbt28(D,shiftn)
 
         CD= C+D
-        Key=permute(CD, PC2)
+        Key=permute(CD, PC2) #into 48 bits
         keys16.append(Key)
     return keys16
