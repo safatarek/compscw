fc4c93f1d52df4c62c80870b047cfceb20aac942
safatarek <se2400552@tkh.edu.eg>
2025-12-10 23:32:23 +0200

    Update csDecryption.py

--- csDecryption.py
+++ csDecryption.py
@@ -14,22 +14,6 @@ def DecryptedBlock(block64b, roundkeys):
     Plain64b=permute(swap, FP)
     return Plain64b
 
-from csEncryption import XOR, IP, FP, permute, fFunction, BitsToText
-
-def DecryptedBlock(block64b, roundkeys):
-    ipresult=permute(block64b, IP)
-    L=ipresult[:32]
-    R=ipresult[32:]
-    for i in range(15, -1, -1):
-        NewL=R
-        fFoutput=fFunction(R, roundkeys[i])
-        NewR=XOR(L, fFoutput)
-        L=NewL
-        R=NewR
-    swap=R+L
-    Plain64b=permute(swap, FP)
-    return Plain64b
-
 def DecryptedMessage(CipherBits, roundkeys):
     PlainText=''
     for i in range(0, len(CipherBits), 64):
