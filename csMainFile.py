a3a53b21292756e684f84bcdc43fcaa0e4c5fe88
safatarek <se2400552@tkh.edu.eg>
2025-12-10 18:17:27 +0200

    Update csMainFile.py

--- csMainFile.py
+++ csMainFile.py
@@ -1,3 +1,21 @@
 from cskeys import genkey, gen16keys
 from csEncryption import EncryptedMessage
 from csDecryption import DecryptedMessage
+
+def Main():
+    key64=genkey()
+    print('Random 64-bit key:')
+    print(key64)
+
+    roundkeys=gen16keys(key64)
+
+    PlainText=input('Please enter your message to get encrypted:')
+
+    CipherBits=EncryptedMessage(PlainText, roundkeys)
+    print('Ciphertext Bits:')
+    print (CipherBits)
+
+    DecryptedText=DecryptedMessage(CipherBits, roundkeys)
+    print('Decrypted Text:')
+    print(DecryptedText)
+    
